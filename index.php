aa6b0ef3cdbb9ae547546b0d53f1b353b4f0826a
Stacced <20029433+Stacced@users.noreply.github.com>
2018-11-07 09:18:15 +0100

    Setting up basics

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>HeroStory | PHP Project</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div>TODO write content</div>
+    </body>
+</html>
