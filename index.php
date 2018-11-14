cae41705e52de6e0d32b391d485a5a995d7fc368
Stacced <20029433+Stacced@users.noreply.github.com>
2018-11-14 13:57:31 +0100

    feat:added stories (not finished)

--- index.php
+++ index.php
@@ -2,20 +2,48 @@
 require 'functions.php';
 
 $historyStep = filter_input(INPUT_POST, 'choice', FILTER_SANITIZE_STRING);
-echo $historyStep;
 $historyValidate = filter_input(INPUT_POST, 'validate', FILTER_SANITIZE_STRING);
 $historyTexts = array( // Struct => array(
                        //           - array(headerText, questionText, leftImageLink, rightImageLink, leftText, rightText, leftChoice, rightChoice)
                        //           );
     "greetings" => ["Bienvenue sur HeroStory !", "A chaque question, il vous sera proposé deux choix."
-        . " Vos choix auront un impact sur la fin de l'histoire !", null, null, null, null, null, null],
+        . " Vos choix auront un impact sur la fin de l'histoire !", null, null, null, null, null, null
+        ],
     
-    "start" => ["Question numéro 1", "Vous êtes tombé dans un puits. Après vous être remis de vos émotions,"
+    "start" => ["Situation de départ", "Vous êtes tombé dans un puits. Après vous être remis de vos émotions,"
         . " vous remarquez une corde posée sur une pierre, et l'entrée d'un tunnel. "
-        . "Que faites-vous ?", "corde", "tunnel", "Vous escaladez", "Vous continuez dans le tunnel", "corde", "tunnel"],
+        . "Que faites-vous ?", "img/corde.jpg", "img/tunnel.jpg", "Vous escaladez", "Vous continuez dans le tunnel", "corde", "tunnel"
+        ],
     
     "corde" => [ "Vous avez choisi d'escalader", "Après être tombé plusieurs fois, vous réussissez à remonter."
-        .  " Vous trouvez un vélo rouillé mais avec des pneus gonflés, et une trotinette" ]
+        .  " Vous trouvez un vélo rouillé mais avec des pneus gonflés, et une trotinette qui semble en bon état", 
+        "img/velo.jpg", "img/trottinette.jpg", "Vous prenez le vélo", "Vous prenez la trottinette", "velo", "trottinette"
+        ],
+    
+    "velo" => [ "Vous avez pris le vélo", "Vous avez parcouru quelques kilomètres, quand soudain le pneu avant de votre vélo crève.",
+                "img/repair.jpg", "img/walk.jpg", "Vous réparez le pneu", "Vous continuez à pied", "reparer", "pied"
+        ],
+    
+    "reparer" => [ "Vous avez réussi !", "C'était le bon choix ! Vous êtes rentré chez vous sans encombre.", null, null, null, null, null, null
+        ],
+    
+    "pied" => [ "Vous êtes mort", "En marchant sur le côté de la route, vous vous êtes fait percuté par une voiture ! Ca vous apprendra à ne pas"
+        . " marcher derrière la rambarde de sécurité !", null, null, null, null, null, null
+        ],
+    
+    "trottinette" => [ "Vous avez pris la trottinette", "Vous vous apercevez que celle-ci est en train de s'endommager au fur et à mesure que vous l'utilisez.",
+            "img/continue.jpg", "img/walk.jpg", "Vous continuez", "Vous abandonnez la trottinette et continuez à pied", "continue", "reparer"
+        ],
+    
+    "continue" => [ "La trottinette casse", "Ce que vous attendiez arriva : la trottinette casse, et un bout de métal vous blesse au ventre. Vous saignez abondamment",
+            "img/wait.jpg", "img/walk.jpg", "Vous attendez en espérant que quelqu'un passe", "Vous marchez le reste du chemin", "wait", "pied"
+        ],
+    
+    "wait" => [ "Heureusement que vous avez attendu", "Par chance, une voiture vous voit et s'arrête, puis vous emmène à l'hôpital !", null, null, null, null, null, null
+        ],
+    
+    "tunnel" => [ "Vous êtes entré dans le tunnel", "Après quelques minutes de marche, vous rencontrez votre premier obstacle : le tunnel se sépare en deux chemins."
+        . " Lequel choisissez-vous ?", "" ]
 );
 
 if (!isset($historyStep) && !isset($historyValidate)) {
@@ -51,11 +79,16 @@ $rightChoice = $historyTexts[$historyStep][7];
             <form action="index.php" method="POST">
                 <?php
                 if (isset($leftImage) && isset($rightImage)) {
-                    echo "<img style='margin-right: 30px' src='img/" . $leftImage . ".jpg' alt='left image'>";
-                    echo "<img style='margin-left: 30px' src='img/" . $rightImage . ".jpg' alt='right image'>";
+                    echo "<div id='idImgContainer'>";
+                    echo "<img src='$leftImage' alt='left image'>";
+                    echo "<img src='$rightImage' alt='right image'>";
+                    echo "</div>";
                     echo "<br>";
-                    echo "<input type=radio name='choice' value='$leftChoice'>$leftText";
-                    echo "<input type=radio name='choice' value='$rightChoice'>$rightText";
+                    
+                    echo "<div id='idRadioContainer'>";
+                    echo "<input type=radio id='idRadioLeft' name='choice' value='$leftChoice'><label for='idRadioLeft'>$leftText</label>";
+                    echo "<input type=radio id='idRadioRight' name='choice' value='$rightChoice'><label for='idRadioRight'>$rightText</label>";
+                    echo "</div>";
                     echo "<br>";
                 }
                 echo "<input type=submit name='validate' value='Continuer'>";
