6da95039236755ff225a9d8c9547267e09388fbf
Stacced <20029433+Stacced@users.noreply.github.com>
2018-11-14 10:53:08 +0100

    feat:dynamic pages working

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php
-session_start();
 require 'functions.php';
 
-$historyStep = 0; // 0 => loaded site for 1st time ; 1 => first question and so on
+$historyStep = filter_input(INPUT_POST, 'choice', FILTER_SANITIZE_STRING);
+echo $historyStep;
+$historyValidate = filter_input(INPUT_POST, 'validate', FILTER_SANITIZE_STRING);
 $historyTexts = array( // Struct => array(
-                       //           - array(headerText, questionText, leftImageLink, rightImageLink)
+                       //           - array(headerText, questionText, leftImageLink, rightImageLink, leftText, rightText, leftChoice, rightChoice)
                        //           );
-    array("Bienvenue sur HeroStory !", "A chaque question, il vous sera proposé deux choix."
-        . " Vos choix auront un impact sur la fin de l'histoire !"),
-    array("Question numéro 1", "Vous êtes tombé dans un puits. Après vous être remis de vos émotions,"
+    "greetings" => ["Bienvenue sur HeroStory !", "A chaque question, il vous sera proposé deux choix."
+        . " Vos choix auront un impact sur la fin de l'histoire !", null, null, null, null, null, null],
+    
+    "start" => ["Question numéro 1", "Vous êtes tombé dans un puits. Après vous être remis de vos émotions,"
         . " vous remarquez une corde posée sur une pierre, et l'entrée d'un tunnel. "
-        . "Que faites-vous ?", "img/corde.jpg", "img/tunnel.jpg")
+        . "Que faites-vous ?", "corde", "tunnel", "Vous escaladez", "Vous continuez dans le tunnel", "corde", "tunnel"],
+    
+    "corde" => [ "Vous avez choisi d'escalader", "Après être tombé plusieurs fois, vous réussissez à remonter."
+        .  " Vous trouvez un vélo rouillé mais avec des pneus gonflés, et une trotinette" ]
 );
-        
-if (!isset($_SESSION['startedHistory'])) {
-    $_SESSION['startedHistory'] = true;
-    $_SESSION['historyStep'] = 0;
-    $historyStep = $_SESSION['historyStep'];
-} else {
-    $_SESSION['historyStep']++;
-    $historyStep = $_SESSION['historyStep'];
+
+if (!isset($historyStep) && !isset($historyValidate)) {
+    $historyStep = "greetings";
+} else if (!isset($historyStep)) {
+    $historyStep = "start";
 }
 
 $questionHeaderText = $historyTexts[$historyStep][0];
 $questionText = $historyTexts[$historyStep][1];
 
+$leftImage = $historyTexts[$historyStep][2];
+$rightImage = $historyTexts[$historyStep][3];
+
+$leftText = $historyTexts[$historyStep][4];
+$rightText = $historyTexts[$historyStep][5];
+
+$leftChoice = $historyTexts[$historyStep][6];
+$rightChoice = $historyTexts[$historyStep][7];
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -38,12 +48,19 @@ $questionText = $historyTexts[$historyStep][1];
         <div id="idQuestion">
             <h2><?= $questionHeaderText ?></h2>
             <h3><?= $questionText ?></h3>
-            <?php
-            if (isset($historyTexts[$historyStep][2]) && isset($historyTexts[$historyStep][3])) {
-                echo "<img src='" . $historyTexts[$historyStep][2] . "' alt='left image'>";
-                echo "<img src='" . $historyTexts[$historyStep][3] . "' alt='right image'>";
-            }
-            ?>
+            <form action="index.php" method="POST">
+                <?php
+                if (isset($leftImage) && isset($rightImage)) {
+                    echo "<img style='margin-right: 30px' src='img/" . $leftImage . ".jpg' alt='left image'>";
+                    echo "<img style='margin-left: 30px' src='img/" . $rightImage . ".jpg' alt='right image'>";
+                    echo "<br>";
+                    echo "<input type=radio name='choice' value='$leftChoice'>$leftText";
+                    echo "<input type=radio name='choice' value='$rightChoice'>$rightText";
+                    echo "<br>";
+                }
+                echo "<input type=submit name='validate' value='Continuer'>";
+                ?>
+            </form>
         </div>
     </body>
 </html>
