6d80ffa40eb63b2b62a42965393842a0231c76e5
Stacced <20029433+Stacced@users.noreply.github.com>
2018-11-28 10:56:54 +0100

    feat:added header gen (could be reversed)

--- index.php
+++ index.php
@@ -9,39 +9,39 @@ $historyTexts = array( // Struct => array(
     "greetings" => ["Bienvenue sur HeroStory !", "A chaque question, il vous sera proposé deux choix."
         . " Vos choix auront un impact sur la fin de l'histoire !", null, null, null, null, null, null
         ],
-    
+
     "start" => ["Situation de départ", "Vous êtes tombé dans un puits. Après vous être remis de vos émotions,"
         . " vous remarquez une corde posée sur une pierre, et l'entrée d'un tunnel. "
         . "Que faites-vous ?", "img/corde.jpg", "img/tunnel.jpg", "Vous escaladez", "Vous continuez dans le tunnel", "corde", "tunnel"
         ],
-    
+
     "corde" => [ "Vous avez choisi d'escalader", "Après être tombé plusieurs fois, vous réussissez à remonter."
-        .  " Vous trouvez un vélo rouillé mais avec des pneus gonflés, et une trotinette qui semble en bon état", 
+        .  " Vous trouvez un vélo rouillé mais avec des pneus gonflés, et une trotinette qui semble en bon état",
         "img/velo.jpg", "img/trottinette.jpg", "Vous prenez le vélo", "Vous prenez la trottinette", "velo", "trottinette"
         ],
-    
+
     "velo" => [ "Vous avez pris le vélo", "Vous avez parcouru quelques kilomètres, quand soudain le pneu avant de votre vélo crève.",
                 "img/repair.jpg", "img/walk.jpg", "Vous réparez le pneu", "Vous continuez à pied", "reparer", "pied"
         ],
-    
+
     "reparer" => [ "Vous avez réussi !", "C'était le bon choix ! Vous êtes rentré chez vous sans encombre.", null, null, null, null, null, null
         ],
-    
+
     "pied" => [ "Vous êtes mort", "En marchant sur le côté de la route, vous vous êtes fait percuté par une voiture ! Ca vous apprendra à ne pas"
         . " marcher derrière la rambarde de sécurité !", null, null, null, null, null, null
         ],
-    
+
     "trottinette" => [ "Vous avez pris la trottinette", "Vous vous apercevez que celle-ci est en train de s'endommager au fur et à mesure que vous l'utilisez.",
             "img/continue.jpg", "img/walk.jpg", "Vous continuez", "Vous abandonnez la trottinette et continuez à pied", "continue", "reparer"
         ],
-    
+
     "continue" => [ "La trottinette casse", "Ce que vous attendiez arriva : la trottinette casse, et un bout de métal vous blesse au ventre. Vous saignez abondamment",
             "img/wait.jpg", "img/walk.jpg", "Vous attendez en espérant que quelqu'un passe", "Vous marchez le reste du chemin", "wait", "pied"
         ],
-    
+
     "wait" => [ "Heureusement que vous avez attendu", "Par chance, une voiture vous voit et s'arrête, puis vous emmène à l'hôpital !", null, null, null, null, null, null
         ],
-    
+
     "tunnel" => [ "Vous êtes entré dans le tunnel", "Après quelques minutes de marche, vous rencontrez votre premier obstacle : le tunnel se sépare en deux chemins."
         . " Lequel choisissez-vous ?", "" ]
 );
@@ -72,6 +72,7 @@ $rightChoice = $historyTexts[$historyStep][7];
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
+    <?= GenerateHeader() ?>
     <body>
         <div id="idQuestion">
             <h2><?= $questionHeaderText ?></h2>
@@ -84,7 +85,7 @@ $rightChoice = $historyTexts[$historyStep][7];
                     echo "<img src='$rightImage' alt='right image'>";
                     echo "</div>";
                     echo "<br>";
-                    
+
                     echo "<div id='idRadioContainer'>";
                     echo "<input type=radio id='idRadioLeft' name='choice' value='$leftChoice'><label for='idRadioLeft'>$leftText</label>";
                     echo "<input type=radio id='idRadioRight' name='choice' value='$rightChoice'><label for='idRadioRight'>$rightText</label>";
